fe3aa06d4f85a878deb9bd4e044124e33e3bc916
Ksenya Lavrinovich <lavrinovich.kg.13@gmail.com>
2015-11-13 23:40:35 +0300

    change method of counting duration

---
+++ ATCEmulator/ATCEmulator/ATC/CallInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATCEmulator
+{
+    public class CallInfo
+    {
+        private static Random random = new Random();
+        public PhoneNumber Source { get; protected set; }
+        public PhoneNumber Target { get; protected set; }
+        public DateTime StartedAt { get; protected set; }
+        public TimeSpan Duration { get; protected set; }
+
+        public CallInfo (PhoneNumber source, PhoneNumber target)
+        {
+            this.Source = source;
+            this.Target = target;
+            this.StartedAt = new DateTime();
+            this.Duration = new TimeSpan(0,0,0);
+        }
+
+        public void SetStartTime(DateTime startTime)
+        {
+            this.StartedAt = startTime;
+        }
+
+        public void SetDuration(TimeSpan duration)
+        {
+            this.Duration = duration;
+        }
+    }
+}
