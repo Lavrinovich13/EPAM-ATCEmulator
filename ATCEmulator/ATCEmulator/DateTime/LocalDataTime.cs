1cc318d61841fa5886c72c08dc47dcb46656f154
Ksenya Lavrinovich <lavrinovich.kg.13@gmail.com>
2015-11-13 23:41:01 +0300

    added classes for datetime

---
+++ ATCEmulator/ATCEmulator/DateTime/LocalDataTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATCEmulator
+{
+    public static class LocalDateTime
+    {
+        private static int Counter = 0;
+        
+        public static DateTime Now
+        {
+            get
+            {
+                Counter++;
+                DateTime dateTimeNow = DateTime.Now;
+                dateTimeNow = dateTimeNow.AddDays(5 * Counter);
+                return dateTimeNow;
+            }
+        }
+    }
+}
---
+++ ATCEmulator/ATCEmulator/DateTime/LocalTimeSpan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATCEmulator
+{
+    public static class LocalTimeSpan
+    {
+        public static TimeSpan Duration(DateTime start, DateTime end)
+        {
+            return new TimeSpan(0, end.Millisecond / 10, 0); 
+        }
+    }
+}
