91d8152c9d404f9d0cc4a254ada7e16a41fbab26
Ksenya Lavrinovich <lavrinovich.kg.13@gmail.com>
2015-11-16 09:59:52 +0300

    add interface

---
+++ ATSEmulator/ATSEmulator/ATS/Interfaces/IAts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATSEmulator
+{
+    public interface IAts
+    {
+        void ConnectToBillingSystem(IBillingSystem billingSystem);
+        void RegisterOnPortEvent(IPort port);
+        void ClearEvents();
+    }
+}
